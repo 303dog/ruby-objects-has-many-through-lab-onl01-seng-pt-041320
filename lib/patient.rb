94999dcdb24d24c1305bc203036b322c461fe8d7
Paul Beckwith <303dog@gmail.com>
2020-05-15 23:27:49 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Patient
+  
+  attr_accessor :name, :date, :
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date)
+    
+  end
+  
+  def appointments
+    
+  end
+  
+  def doctors
+    
+  end
+end
