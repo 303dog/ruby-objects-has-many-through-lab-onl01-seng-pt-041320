9c74ace21d2b981f1a5b37f880a6f4601449c69a
Paul Beckwith <303dog@gmail.com>
2020-05-15 23:41:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,24 @@
 class Patient
-  
-  attr_accessor :name, :date 
-  
-  @@all = []
-  
+
+  attr_reader :name, :appointment, :doctor
+
   def initialize(name)
     @name = name
-    @@all << name
   end
-  
-  def self.all
-    @@all
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
   end
-  
-  def new_appointment(date)
-    
-  end
-  
+
   def appointments
-    
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
-  
+
   def doctors
-    
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
   end
 end
