376bcd8865bc936b2a47b3bd23f4f5d57ef3d603
Paul Beckwith <303dog@gmail.com>
2020-05-15 23:14:08 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist 
+  
+  
+  @@all = []
+  
+  def initailize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+
+  end
+  
+  def songs 
+    song.each.select 
+  end
+    
+  def genres 
+    
+  end
+  
+    
+  end 
+end
